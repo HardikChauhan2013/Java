2645f22513365eba861308a406454270b6136a3e
hardikchauhan <hardik.chauhan@hotmail.com>
2020-05-09 13:38:14 +0530

    JWT demo

---
+++ Simple_JWT_Demo/src/main/java/com/example/demo/SimpleJwtDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleJwtDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleJwtDemoApplication.class, args);
+	}
+
+}
